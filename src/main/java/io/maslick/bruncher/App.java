6ad04df10c06b3173192e0733c75f3b987d1849e
Pavel Maslov <pavel.maslov@ineor.si>
2019-04-11 01:17:50 +0200

    implemented best feature

--- src/main/java/io/maslick/bruncher/App.java
+++ src/main/java/io/maslick/bruncher/App.java
@@ -2,6 +2,6 @@ package io.maslick.bruncher;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello world???");
+		System.out.println("Best program on Earth!");
 	}
 }
