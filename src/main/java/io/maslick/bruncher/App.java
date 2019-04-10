e0b4dbf66c507fd3193aebf053c89a65c53b2cc5
Pavel Maslov <pavel.masloff@gmail.com>
2019-04-11 01:54:52 +0200

    Release/4321 (#8)
    * implemented feature3 (#2)
    
    * added travis.yaml
    
    * fixed travis script
    
    * implemented best feature
    
    * added rc stage
    
    * added feture 432 (#7)

--- src/main/java/io/maslick/bruncher/App.java
+++ src/main/java/io/maslick/bruncher/App.java
@@ -2,6 +2,6 @@ package io.maslick.bruncher;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Best program on Earth!");
+		System.out.println("Best program on the planet Earth!!!");
 	}
 }
