07b61747695a97a9108e0376a3e96115242335dc
Pavel Maslov <pavel.maslov@ineor.si>
2019-04-12 11:43:26 +0200

    hotfix 2

--- src/main/java/io/maslick/bruncher/App.java
+++ src/main/java/io/maslick/bruncher/App.java
@@ -2,6 +2,6 @@ package io.maslick.bruncher;
 
 public class App {
 	public static void main(String[] args) {
-    	System.out.println("Best program in the Universe? Bruncher!");
+    	System.out.println("Best program in the Universe? Bruncher!!");
 	}
 }
