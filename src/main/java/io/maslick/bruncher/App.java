06b9b98a76b32bc36d0cbc4aef10d7e015337f30
Pavel Maslov <pavel.masloff@gmail.com>
2019-04-12 12:17:52 +0200

    Release 100 (#24)

--- src/main/java/io/maslick/bruncher/App.java
+++ src/main/java/io/maslick/bruncher/App.java
@@ -3,5 +3,6 @@ package io.maslick.bruncher;
 public class App {
 	public static void main(String[] args) {
     	System.out.println("Best program in the Universe? Bruncher!!!");
+		System.out.println("But here is helloworld!");
 	}
 }
